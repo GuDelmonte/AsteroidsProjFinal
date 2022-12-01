a2d13e6db390080195d1b1d4c4dfa20b44b8ffb8
TaynaraR <cc21114@g.unicamp.br>
2022-12-01 14:53:29 -0300

    att do jogo

--- main.py
+++ main.py
@@ -7,26 +7,27 @@ pygame.init()
 sw = 800
 sh = 800
 
+# adicionando imagens
 bg = pygame.image.load('asteroidsPics/starbg.png')
 alienImg = pygame.image.load('asteroidsPics/alienShip.png')
 playerRocket = pygame.image.load('asteroidsPics/spaceRocket.png')
-star = pygame.image.load('asteroidsPics/star.png')
+estrela = pygame.image.load('asteroidsPics/star.png')
 asteroid50 = pygame.image.load('asteroidsPics/asteroid50.png')
 asteroid100 = pygame.image.load('asteroidsPics/asteroid100.png')
 asteroid150 = pygame.image.load('asteroidsPics/asteroid150.png')
 
 pygame.display.set_caption('Asteroids')
-win = pygame.display.set_mode((sw, sh))
-relogio = pygame.time.Clock()
+ganhar = pygame.display.set_mode((sw, sh))
+timer = pygame.time.Clock()
 
-gameover = False
+fimdejogo = False
 vidas = 3
 pontuacao = 0
 rapidFire = False
 rfStart = -1
-highScore = 0
+maximapontuacao = 0
 
-
+#funçoes da nave
 class Player(object):
     def __init__(self):
         self.img = playerRocket
@@ -42,11 +43,13 @@ class Player(object):
         self.sine = math.sin(math.radians(self.angle + 90))
         self.head = (self.x + self.cosine * self.w//2, self.y - self.sine * self.h//2)
 
-    def draw(self, win):
-        #win.blit(self.img, [self.x, self.y, self.w, self.h])
-        win.blit(self.rotatedSurf, self.rotatedRect)
+    def draw(self, ganhar):
+        #ganhar.blit(self.img, [self.x, self.y, self.w, self.h])
+        ganhar.blit(self.rotatedSurf, self.rotatedRect)
 
-    def turnLeft(self):
+# rotaçoes da nave
+
+    def paraEsquerda(self):
         self.angle += 5
         self.rotatedSurf = pygame.transform.rotate(self.img, self.angle)
         self.rotatedRect = self.rotatedSurf.get_rect()
@@ -55,7 +58,7 @@ class Player(object):
         self.sine = math.sin(math.radians(self.angle + 90))
         self.head = (self.x + self.cosine * self.w//2, self.y - self.sine * self.h//2)
 
-    def turnRight(self):
+    def paraDireita(self):
         self.angle -= 5
         self.rotatedSurf = pygame.transform.rotate(self.img, self.angle)
         self.rotatedRect = self.rotatedSurf.get_rect()
@@ -64,7 +67,7 @@ class Player(object):
         self.sine = math.sin(math.radians(self.angle + 90))
         self.head = (self.x + self.cosine * self.w//2, self.y - self.sine * self.h//2)
 
-    def moveForward(self):
+    def paraFrente(self):
         self.x += self.cosine * 6
         self.y -= self.sine * 6
         self.rotatedSurf = pygame.transform.rotate(self.img, self.angle)
@@ -74,7 +77,9 @@ class Player(object):
         self.sine = math.sin(math.radians(self.angle + 90))
         self.head = (self.x + self.cosine * self.w // 2, self.y - self.sine * self.h // 2)
 
-    def updateLocation(self):
+#Atualizar a localização da nave
+
+    def attLocalizacao(self):
         if self.x > sw + 50:
             self.x = 0
         elif self.x < 0 - self.w:
@@ -84,7 +89,9 @@ class Player(object):
         elif self.y > sh + 50:
             self.y = 0
 
-class Bullet(object):
+# Estrutura da Bala
+class Bala(object):
+
     def __init__(self):
         self.point = player.head
         self.x, self.y = self.point
@@ -99,13 +106,16 @@ class Bullet(object):
         self.x += self.xv
         self.y -= self.yv
 
-    def draw(self, win):
-        pygame.draw.rect(win, (255, 255, 255), [self.x, self.y, self.w, self.h])
+    def draw(self, ganhar):
+        pygame.draw.rect(ganhar, (255, 255, 255), [self.x, self.y, self.w, self.h])
+
+# Quando a nave sair da tela ela retorna pelo outro lado 
 
     def checkOffScreen(self):
         if self.x < -50 or self.x > sw or self.y > sh or self.y < -50:
             return True
 
+# Funções Asteroids
 class Asteroid(object):
     def __init__(self, rank):
         self.rank = rank
@@ -130,12 +140,13 @@ class Asteroid(object):
         self.xv = self.xdir * random.randrange(1,3)
         self.yv = self.ydir * random.randrange(1,3)
 
-    def draw(self, win):
-        win.blit(self.image, (self.x, self.y))
+    def draw(self, ganhar):
+        ganhar.blit(self.image, (self.x, self.y))
 
-class Star(object):
+# funções da estrela
+class Estrela(object):
     def __init__(self):
-        self.img = star
+        self.img = estrela
         self.w = self.img.get_width()
         self.h = self.img.get_height()
         self.ranPoint = random.choice([(random.randrange(0, sw - self.w), random.choice([-1 * self.h - 5, sh + 5])),
@@ -152,16 +163,17 @@ class Star(object):
         self.xv = self.xdir * 2
         self.yv = self.ydir * 2
 
-    def draw(self, win):
-        win.blit(self.img, (self.x, self.y))
+    def draw(self, ganhar):
+        ganhar.blit(self.img, (self.x, self.y))
 
+# funções da nave alien
 class Alien(object):
     def __init__(self):
         self.img = alienImg
         self.w = self.img.get_width()
         self.h = self.img.get_height()
         self.ranPoint = random.choice([(random.randrange(0, sw - self.w), random.choice([-1 * self.h - 5, sh + 5])),
-                                       (random.choice([-1 * self.w - 5, sw + 5]), random.randrange(0, sh - self.h))])
+                                    (random.choice([-1 * self.w - 5, sw + 5]), random.randrange(0, sh - self.h))])
         self.x, self.y = self.ranPoint
         if self.x < sw//2:
             self.xdir = 1
@@ -174,11 +186,11 @@ class Alien(object):
         self.xv = self.xdir * 2
         self.yv = self.ydir * 2
 
-    def draw(self, win):
-        win.blit(self.img, (self.x, self.y))
+    def draw(self, ganhar):
+        ganhar.blit(self.img, (self.x, self.y))
 
-
-class AlienBullet(object):
+# Funções da bala da nave alien 
+class AlienBala(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -190,62 +202,62 @@ class AlienBullet(object):
         self.xv = self.dx * 5
         self.yv = self.dy * 5
 
-    def draw(self, win):
-        pygame.draw.rect(win, (255, 255, 255), [self.x, self.y, self.w, self.h])
-
-
-
+    def draw(self, ganhar):
+        pygame.draw.rect(ganhar, (255, 255, 255), [self.x, self.y, self.w, self.h])
 
 def redrawGameWindow():
-    win.blit(bg, (0,0))
+    ganhar.blit(bg, (0,0))
     font = pygame.font.SysFont('arial',30)
-    livesText = font.render('Lives: ' + str(lives), 1, (255, 255, 255))
+    vidasText = font.render('vidas: ' + str(vidas), 1, (255, 255, 255))
     playAgainText = font.render('Press Tab to Play Again', 1, (255,255,255))
-    scoreText = font.render('Score: ' + str(score), 1, (255,255,255))
-    highScoreText = font.render('High Score: ' + str(highScore), 1, (255, 255, 255))
+    pontuacaoText = font.render('pontuacao: ' + str(pontuacao), 1, (255,255,255))
+    maximapontuacaoText = font.render('maxima pontuacao: ' + str(maximapontuacao), 1, (255, 255, 255))
 
-    player.draw(win)
+# Para cada bala dentro do playerBalas ele desenha uma nova bala
+
+    player.draw(ganhar)
     for a in asteroids:
-        a.draw(win)
-    for b in playerBullets:
-        b.draw(win)
-    for s in stars:
-        s.draw(win)
+        a.draw(ganhar)
+    for b in playerBalas:
+        b.draw(ganhar)
+    for s in estrelas:
+        s.draw(ganhar)
     for a in aliens:
-        a.draw(win)
-    for b in alienBullets:
-        b.draw(win)
+        a.draw(ganhar)
+    for b in alienBalas:
+        b.draw(ganhar)
 
     if rapidFire:
-        pygame.draw.rect(win, (0, 0, 0), [sw//2 - 51, 19, 102, 22])
-        pygame.draw.rect(win, (255, 255, 255), [sw//2 - 50, 20, 100 - 100*(count - rfStart)/500, 20])
+        pygame.draw.rect(ganhar, (0, 0, 0), [sw//2 - 51, 19, 102, 22])
+        pygame.draw.rect(ganhar, (255, 255, 255), [sw//2 - 50, 20, 100 - 100*(count - rfStart)/500, 20])
 
-    if gameover:
-        win.blit(playAgainText, (sw//2-playAgainText.get_width()//2, sh//2 - playAgainText.get_height()//2))
-    win.blit(scoreText, (sw- scoreText.get_width() - 25, 25))
-    win.blit(livesText, (25, 25))
-    win.blit(highScoreText, (sw - highScoreText.get_width() -25, 35 + scoreText.get_height()))
+    if fimdejogo:
+        ganhar.blit(playAgainText, (sw//2-playAgainText.get_width()//2, sh//2 - playAgainText.get_height()//2))
+    ganhar.blit(pontuacaoText, (sw- pontuacaoText.get_width() - 25, 25))
+    ganhar.blit(vidasText, (25, 25))
+    ganhar.blit(maximapontuacaoText, (sw - maximapontuacaoText.get_width() -25, 35 + pontuacaoText.get_height()))
     pygame.display.update()
 
 
-
 player = Player()
-playerBullets = []
+playerBalas = []
 asteroids = []
 count = 0
-stars = []
+estrelas = []
 aliens = []
-alienBullets = []
+alienBalas = []
 run = True
+
 while run:
-    clock.tick(60)
+    
+    timer.tick(60)
     count += 1
-    if not gameover:
+    if not fimdejogo:
         if count % 50 == 0:
             ran = random.choice([1,1,1,2,2,3])
             asteroids.append(Asteroid(ran))
         if count % 1000 == 0:
-            stars.append(Star())
+            estrelas.append(estrela())
         if count % 750 == 0:
             aliens.append(Alien())
         for i, a in enumerate(aliens):
@@ -254,31 +266,32 @@ while run:
             if a.x > sw + 150 or a.x + a.w < -100 or a.y > sh + 150 or a.y + a.h < -100:
                 aliens.pop(i)
             if count % 60 == 0:
-                alienBullets.append(AlienBullet(a.x + a.w//2, a.y + a.h//2))
+                alienBalas.append(AlienBala(a.x + a.w//2, a.y + a.h//2))
 
-            for b in playerBullets:
+# ao acertar o asteroid ele ganha uma pontuação 
+            for b in playerBalas:
                 if (b.x >= a.x and b.x <= a.x + a.w) or b.x + b.w >= a.x and b.x + b.w <= a.x + a.w:
                     if (b.y >= a.y and b.y <= a.y + a.h) or b.y + b.h >= a.y and b.y + b.h <= a.y + a.h:
                         aliens.pop(i)
-                        if isSoundOn:
-                            bangLargeSound.play()
-                        score += 50
+                    
+                    # tratar depois colocar para o led acender aqui
+                        pontuacao += 50
                         break
 
-        for i, b in enumerate(alienBullets):
+        for i, b in enumerate(alienBalas):
             b.x += b.xv
             b.y += b.yv
             if (b.x >= player.x - player.w//2 and b.x <= player.x + player.w//2) or b.x + b.w >= player.x - player.w//2 and b.x + b.w <= player.x + player.w//2:
                 if (b.y >= player.y-player.h//2 and b.y <= player.y + player.h//2) or b.y + b.h >= player.y - player.h//2 and b.y + b.h <= player.y + player.h//2:
-                    lives -= 1
-                    alienBullets.pop(i)
+                    vidas -= 1
+                    alienBalas.pop(i)
                     break
 
-        player.updateLocation()
-        for b in playerBullets:
+        player.attLocalizacao()
+        for b in playerBalas:
             b.move()
             if b.checkOffScreen():
-                playerBullets.pop(playerBullets.index(b))
+                playerBalas.pop(playerBalas.index(b))
 
 
         for a in asteroids:
@@ -287,20 +300,19 @@ while run:
 
             if (a.x >= player.x - player.w//2 and a.x <= player.x + player.w//2) or (a.x + a.w <= player.x + player.w//2 and a.x + a.w >= player.x - player.w//2):
                 if(a.y >= player.y - player.h//2 and a.y <= player.y + player.h//2) or (a.y  +a.h >= player.y - player.h//2 and a.y + a.h <= player.y + player.h//2):
-                    lives -= 1
+                    vidas -= 1
                     asteroids.pop(asteroids.index(a))
-                    if isSoundOn:
-                        bangLargeSound.play()
+                    
                     break
 
-            # bullet collision
-            for b in playerBullets:
+            # Bala colisao
+            
+            for b in playerBalas:
                 if (b.x >= a.x and b.x <= a.x + a.w) or b.x + b.w >= a.x and b.x + b.w <= a.x + a.w:
                     if (b.y >= a.y and b.y <= a.y + a.h) or b.y + b.h >= a.y and b.y + b.h <= a.y + a.h:
                         if a.rank == 3:
-                            if isSoundOn:
-                                bangLargeSound.play()
-                            score += 10
+                        
+                            pontuacao += 10
                             na1 = Asteroid(2)
                             na2 = Asteroid(2)
                             na1.x = a.x
@@ -310,9 +322,8 @@ while run:
                             asteroids.append(na1)
                             asteroids.append(na2)
                         elif a.rank == 2:
-                            if isSoundOn:
-                                bangSmallSound.play()
-                            score += 20
+                            
+                            pontuacao += 20
                             na1 = Asteroid(1)
                             na2 = Asteroid(1)
                             na1.x = a.x
@@ -322,72 +333,73 @@ while run:
                             asteroids.append(na1)
                             asteroids.append(na2)
                         else:
-                            score += 30
-                            if isSoundOn:
-                                bangSmallSound.play()
+                            pontuacao += 30
+                            
                         asteroids.pop(asteroids.index(a))
-                        playerBullets.pop(playerBullets.index(b))
+                        playerBalas.pop(playerBalas.index(b))
                         break
 
-        for s in stars:
+        for s in estrelas:
             s.x += s.xv
             s.y += s.yv
             if s.x < -100 - s.w or s.x > sw + 100 or s.y > sh + 100 or s.y < -100 - s.h:
-                stars.pop(stars.index(s))
+                estrelas.pop(estrelas.index(s))
                 break
-            for b in playerBullets:
+            for b in playerBalas:
                 if (b.x >= s.x and b.x <= s.x + s.w) or b.x + b.w >= s.x and b.x + b.w <= s.x + s.w:
                     if (b.y >= s.y and b.y <= s.y + s.h) or b.y + b.h >= s.y and b.y + b.h <= s.y + s.h:
                         rapidFire = True
                         rfStart = count
-                        stars.pop(stars.index(s))
-                        playerBullets.pop(playerBullets.index(b))
+                        estrelas.pop(estrelas.index(s))
+                        playerBalas.pop(playerBalas.index(b))
                         break
 
-        if lives <= 0:
-            gameover = True
+        if vidas <= 0:
+            fimdejogo = True
 
         if rfStart != -1:
             if count - rfStart > 500:
                 rapidFire = False
                 rfStart = -1
 
+# controles do jogo
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            player.turnLeft()
+            player.paraEsquerda()
         if keys[pygame.K_RIGHT]:
-            player.turnRight()
+            player.paraDireita()
         if keys[pygame.K_UP]:
-            player.moveForward()
+            player.paraFrente()
         if keys[pygame.K_SPACE]:
             if rapidFire:
-                playerBullets.append(Bullet())
-                if isSoundOn:
-                    shoot.play()
+                playerBalas.append(Bala())
+                
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                if not gameover:
+                if not fimdejogo:
                     if not rapidFire:
-                        playerBullets.append(Bullet())
-                        if isSoundOn:
+                        playerBalas.append(Bala())
+                        
+            if event.key == pygame.K_m:
+                if isSoundOn:
                             shoot.play()
             if event.key == pygame.K_m:
                 isSoundOn = not isSoundOn
             if event.key == pygame.K_TAB:
-                if gameover:
-                    gameover = False
-                    lives = 3
+                if fimdejogo:
+                    fimdejogo = False
+                    vidas = 3
                     asteroids.clear()
                     aliens.clear()
-                    alienBullets.clear()
-                    stars.clear()
-                    if score > highScore:
-                        highScore = score
-                    score = 0
+                    alienBalas.clear()
+                    estrelas.clear()
+                    if pontuacao > maximapontuacao:
+                        maximapontuacao = pontuacao
+                    pontuacao = 0
 
     redrawGameWindow()
 pygame.quit()
